515770c6a1408848a529d8a3377e6e780747d3a2
Brian van de Velde <brianvandevelde@008.050.092.145.hva.nl>
2018-12-10 09:23:05 +0100

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Brian van de Velde on 10-12-18.
+//  Copyright © 2018 Brian van de Velde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
